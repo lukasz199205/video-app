cc1f7e911b2787f325e69ed81a9d8f4389140a04
Lukasz <lukasz199205@gmail.com>
2022-01-03 14:40:03 +0100

    FrontController added
    added methods such us:
    -index()
    -videoList()
    -videoDetails()
    -searchResults()
    -pricing()
    -register()
    -login()
    -payment()
    with only basic view render (without any logic)

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    /**
+     * @Route("/", name="main_page")
+     */
+    public function index(): Response
+    {
+        return $this->render('front/index.html.twig');
+    }
+
+    /**
+     * @Route("/video-list", name="video_list")
+     */
+    public function videoList(): Response
+    {
+        return $this->render('front/video_list.html.twig');
+    }
+
+    /**
+     * @Route("/video-details", name="video_details")
+     */
+    public function videoDetails(): Response
+    {
+        return $this->render('front/video_details.html.twig');
+    }
+
+    /**
+     * @Route("/search-results", methods={"POST"}, name="search_results")
+     */
+    public function searchResults(): Response
+    {
+        return $this->render('front/search_results.html.twig');
+    }
+
+    /**
+     * @Route("/pricing", name="pricing")
+     */
+    public function pricing(): Response
+    {
+        return $this->render('front/pricing.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(): Response
+    {
+        return $this->render('front/register.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(): Response
+    {
+        return $this->render('front/login.html.twig');
+    }
+
+    /**
+     * @Route("/payment", name="payment")
+     */
+    public function payment(): Response
+    {
+        return $this->render('front/payment.html.twig');
+    }
+
+}
