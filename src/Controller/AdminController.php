9472c3642b6bd2f2dcf891ed2a221748b779380d
Lukasz <lukasz199205@gmail.com>
2022-01-03 14:42:29 +0100

    AdminController added
    added methods such us:
    -index()
    -categories()
    -videos()
    -uploadVideo()
    -users()
    -editCategory()
    with only basic view render (without any logic)

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_main_page")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/my_profile.html.twig');
+    }
+
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categories(): Response
+    {
+        return $this->render('admin/categories.html.twig');
+    }
+
+    /**
+     * @Route("/videos", name="videos")
+     */
+    public function videos(): Response
+    {
+        return $this->render('admin/videos.html.twig');
+    }
+
+    /**
+     * @Route("/upload-video", name="upload_video")
+     */
+    public function uploadVideo(): Response
+    {
+        return $this->render('admin/upload_video.html.twig');
+    }
+
+    /**
+     * @Route("/users", name="users")
+     */
+    public function users(): Response
+    {
+        return $this->render('admin/users.html.twig');
+    }
+
+    /**
+     * @Route("/edit-category", name="edit_category")
+     */
+    public function editCategory(): Response
+    {
+        return $this->render('admin/edit_category.html.twig');
+    }
+
+}
