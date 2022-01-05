96179f3f4ca5ba453e17851b49bae33c51d4ba12
Lukasz <lukasz199205@gmail.com>
2022-01-05 16:13:16 +0100

    CategoryTreeAbstract add
    this function will allow to build a tree for nested categories.
    Also shows an application of Singleton pattern (getCategories())

---
+++ src/Utils/AbstractClasses/CategoryTreeAbstract.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Utils\AbstractClasses;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+abstract class CategoryTreeAbstract {
+
+    public $categoriesArrayFromDb;
+    protected static $dbConnection;
+
+    public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator)
+    {
+        $this->entityManager = $entityManager;
+        $this->urlGenerator = $urlGenerator;
+        $this->categoriesArrayFromDb = $this->getCategories();
+    }
+
+    abstract public function getCategoryList(array $categories_array);
+
+    public function buildTree(int $parent_id = null): array
+    {
+        $subcategory = [];
+        foreach ($this->categoriesArrayFromDb as $category) {
+            if ($category['parent_id'] == $parent_id) {
+                $children = $this->buildTree($category['id']);
+                if ($children) {
+                    $category['children'] = $children;
+                }
+                $subcategory[] = $category;
+            }
+        }
+        return $subcategory;
+    }
+
+    private function getCategories(): array //singleton pattern
+    {
+        if(self::$dbConnection){
+            return self::$dbConnection;
+        }
+        else{
+            $conn = $this->entityManager->getConnection();
+            $sql = 'SELECT * FROM categories';
+            $stmt = $conn->executeQuery($sql);
+            return self::$dbConnection =  $stmt->fetchAllAssociative();
+        }
+
+    }
+
+}
