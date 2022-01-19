bca263012968050cd544950ffbdd32d5f30ca5fc
Lukasz <lukasz199205@gmail.com>
2022-01-19 13:54:41 +0100

    Videos displayed dynamically

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Video;
 use App\Utils\CategoryTreeFrontPage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,8 +25,11 @@ class FrontController extends AbstractController
     public function videoList($id, CategoryTreeFrontPage $categories): Response
     {
         $categories->getCategoryListAndParent($id);
+        $videos = $this->getDoctrine()->getRepository(Video::class)
+            ->findAll();
         return $this->render('front/video_list.html.twig', [
-            'subcategories' => $categories
+            'subcategories' => $categories,
+            'videos' => $videos
         ]);
     }
 
