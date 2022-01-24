0f55c36314ada6fd5ec700c8c1b5853c5ca465f6
Lukasz <lukasz199205@gmail.com>
2022-01-24 12:25:16 +0100

    Added test for FrontControllerVideoTest
    testResultsFound() created

--- tests/Controllers/Front/FrontControllerVideoTest.php
+++ tests/Controllers/Front/FrontControllerVideoTest.php
@@ -20,4 +20,19 @@ class FrontControllerVideoTest extends WebTestCase
 
         $this->assertStringContainsString('No results were found', $crawler->filter('h1')->text());
     }
+
+    public function testResultsFound()
+    {
+        $client = static::createClient();
+        $client->followRedirects();
+
+        $crawler = $client->request('GET', '/');
+
+        $form = $crawler->selectButton('Search video')->form([
+            'query' => 'Movies',
+        ]);
+        $crawler = $client->submit($form);
+
+        $this->assertGreaterThan(4, $crawler->filter('h3')->count());
+    }
 }
