a40a9d56cb86ae1d077c4e5c4e7d03d1c2eed91f
Lukasz <lukasz199205@gmail.com>
2022-01-24 12:30:31 +0100

    test for Video sorting added

--- tests/Controllers/Front/FrontControllerVideoTest.php
+++ tests/Controllers/Front/FrontControllerVideoTest.php
@@ -35,4 +35,24 @@ class FrontControllerVideoTest extends WebTestCase
 
         $this->assertGreaterThan(4, $crawler->filter('h3')->count());
     }
+
+    public function testSorting()
+    {
+        $client = static::createClient();
+        $client->followRedirects();
+
+        $crawler = $client->request('GET', '/');
+
+        $form = $crawler->selectButton('Search video')->form([
+            'query' => 'Movies',
+        ]);
+        $crawler = $client->submit($form);
+
+        $form = $crawler->filter('#form-sorting')->form([
+            'sortby' => 'desc'
+        ]);
+        $crawler = $client->submit($form);
+
+        $this->assertEquals('Movies 9', $crawler->filter('h3')->first()->text());
+    }
 }
