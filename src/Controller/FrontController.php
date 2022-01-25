31fb4e5be4e5823cb4579b10be6f6552c3507928
Lukasz <lukasz199205@gmail.com>
2022-01-25 11:06:47 +0100

    Logout route and AuthenticationUtils in login route added

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class FrontController extends AbstractController
 {
@@ -83,9 +84,19 @@ class FrontController extends AbstractController
     /**
      * @Route("/login", name="login")
      */
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('front/login.html.twig');
+        return $this->render('front/login.html.twig', [
+            'error' => $authenticationUtils->getLastAuthenticationError()
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(): void
+    {
+        throw new \Exception('This should never be reached');
     }
 
     /**
