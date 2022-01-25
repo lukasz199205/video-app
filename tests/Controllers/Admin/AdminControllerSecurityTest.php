91e217183f48546762e1f3fe993df758cc7a7a8c
Lukasz <lukasz199205@gmail.com>
2022-01-25 15:52:22 +0100

    AdminControllerSecurityTest added
    with test cases:
    - testAccessDeniedForRegularUsers,
    - testAdminSu

---
+++ tests/Controllers/Admin/AdminControllerSecurityTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests\Controllers\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminControllerSecurityTest extends WebTestCase
+{
+    /**
+     * @param string $httpMethod
+     * @param string $url
+     * @return void
+     * @dataProvider getUrlsForRegularUsers
+     */
+    public function testAccessDeniedForRegularUsers(string $httpMethod, string $url)
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'jd@symf4.loc',
+            'PHP_AUTH_PW' => 'passw'
+        ]);
+        $client->request($httpMethod, $url);
+
+        $this->assertSame(Response::HTTP_FORBIDDEN, $client->getResponse()->getStatusCode());
+    }
+
+    public function getUrlsForRegularUsers()
+    {
+        yield ['GET', '/admin/su/categories'];
+        yield ['GET', '/admin/su/edit-category/1'];
+        yield ['GET', '/admin/su/delete-category/1'];
+        yield ['GET', '/admin/su/users'];
+        yield ['GET', '/admin/su/upload-video'];
+    }
+
+    public function testAdminSu()
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'jw@symf4.loc',
+            'PHP_AUTH_PW' => 'passw'
+        ]);
+        $crawler = $client->request('GET', '/admin/su/categories');
+
+        $this->assertSame('Categories list', $crawler->filter('h2')->text());
+
+    }
+}
