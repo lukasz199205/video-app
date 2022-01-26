97e5193b60a3efdf3bb90848022e1454f1cfc976
Lukasz <lukasz199205@gmail.com>
2022-01-26 13:52:26 +0100

    Test refoctored using trait
    for repeated function setUp() and tearDown()

--- tests/Controllers/Admin/AdminControllerCategoriesTest.php
+++ tests/Controllers/Admin/AdminControllerCategoriesTest.php
@@ -4,27 +4,11 @@ namespace App\tests;
 
 use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Tests\Rollback;
 
 class AdminControllerCategoriesTest extends WebTestCase
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-        $this->client = static::createClient([],[
-            'PHP_AUTH_USER' => 'jw@symf4.loc',
-            'PHP_AUTH_PW' => 'passw'
-        ]);
-        $this->client->disableReboot();
-
-        $this->entityManager = $this->client->getContainer()->get('doctrine.orm.entity_manager');
-    }
-
-    public function tearDown(): void
-    {
-        parent::tearDown();
-        $this->entityManager->close();
-        $this->entityManager = null; // avoid memory leaks
-    }
+    use Rollback;
 
     public function testTextOnPage()
     {
