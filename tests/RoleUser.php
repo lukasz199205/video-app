7ee9469443a887346508021c73d3dba6fc120232
Lukasz <lukasz199205@gmail.com>
2022-01-27 13:10:37 +0100

    Added setups for admin/user roles in tests

---
+++ tests/RoleUser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests;
+
+trait RoleUser
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->client = static::createClient([],[
+            'PHP_AUTH_USER' => 'jd@symf4.loc',
+            'PHP_AUTH_PW' => 'passw'
+        ]);
+
+        $this->entityManager = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        $this->entityManager->close();
+        $this->entityManager = null; // avoid memory leaks
+    }
+}
