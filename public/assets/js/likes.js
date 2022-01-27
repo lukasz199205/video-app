7c0e6f9e4af56eb14af3c64877e4de4dad34a12a
Lukasz <lukasz199205@gmail.com>
2022-01-27 11:51:41 +0100

    likes.js rollback to regular version due to problem with minification

--- public/assets/js/likes.js
+++ public/assets/js/likes.js
@@ -1,1 +1,54 @@
-(self.webpackChunkvideo_app=self.webpackChunkvideo_app||[]).push([[310],{154:()=>{$(document).ready((function(){$(".userLikesVideo").show(),$(".userDoesNotLikeVideo").show(),$(".noActionYet").show(),$(".toogle-likes").on("click",(function(i){i.preventDefault();var e=$(i.currentTarget);$.ajax({method:"POST",url:e.attr("href")}).done((function(i){switch(i.action){case"liked":var e=$(".number-of-likes-"+i.id),d=parseInt(e.html().replace(/\D/g,""))+1;e.html("("+d+")"),$(".likes-video-id-"+i.id).show(),$(".dislikes-video-id-"+i.id).hide(),$(".video-id-"+i.id).hide();break;case"disliked":var s=$(".number-of-dislikes-"+i.id),o=parseInt(s.html().replace(/\D/g,""))+1;s.html("("+o+")"),$(".dislikes-video-id-"+i.id).show(),$(".likes-video-id-"+i.id).hide(),$(".video-id-"+i.id).hide();break;case"undo liked":e=$(".number-of-likes-"+i.id),d=parseInt(e.html().replace(/\D/g,""))-1;e.html("("+d+")"),$(".video-id-"+i.id).show(),$(".dislikes-video-id-"+i.id).hide(),$(".likes-video-id-"+i.id).hide();break;case"undo disliked":s=$(".number-of-dislikes-"+i.id),o=parseInt(s.html().replace(/\D/g,""))-1;s.html("("+o+")"),$(".video-id-"+i.id).show(),$(".dislikes-video-id-"+i.id).hide(),$(".likes-video-id-"+i.id).hide()}}))}))}))}},i=>{var e;e=154,i(i.s=e)}]);
+$(document).ready(function () {
+    $('.userLikesVideo').show();
+    $('.userDoesNotLikeVideo').show();
+    $('.noActionYet').show();
+
+    $('.toogle-likes').on('click', function (e) {
+        e.preventDefault();
+        let $link = $(e.currentTarget);
+        $.ajax({
+            method: 'POST',
+            url: $link.attr('href')
+        }).done(function (data) {
+            switch (data.action) {
+                case 'liked':
+                    var number_of_likes_str =  $('.number-of-likes-' + data.id);
+                    var number_of_likes = parseInt( number_of_likes_str.html().replace(/\D/g,'') ) + 1;
+                    number_of_likes_str.html('(' + number_of_likes + ')');
+                    $('.likes-video-id-'+data.id).show();
+                    $('.dislikes-video-id-'+data.id).hide();
+                    $('.video-id-'+data.id).hide();
+                    break;
+
+                case 'disliked':
+                    var number_of_dislikes_str =  $('.number-of-dislikes-' + data.id);
+                    var number_of_dislikes = parseInt( number_of_dislikes_str.html().replace(/\D/g,'') ) + 1;
+                    number_of_dislikes_str.html('(' + number_of_dislikes + ')');
+                    $('.dislikes-video-id-'+data.id).show();
+                    $('.likes-video-id-'+data.id).hide();
+                    $('.video-id-'+data.id).hide();
+                    break;
+
+                case 'undo liked':
+                    var number_of_likes_str =  $('.number-of-likes-' + data.id);
+
+                    var number_of_likes = parseInt( number_of_likes_str.html().replace(/\D/g,'') ) - 1;
+                    number_of_likes_str.html('(' + number_of_likes + ')');
+                    $('.video-id-'+data.id).show();
+                    $('.dislikes-video-id-'+data.id).hide();
+                    $('.likes-video-id-'+data.id).hide();
+                    break;
+
+                case 'undo disliked':
+                    var number_of_dislikes_str =  $('.number-of-dislikes-' + data.id);
+                    var number_of_dislikes = parseInt( number_of_dislikes_str.html().replace(/\D/g,'') ) - 1;
+                    number_of_dislikes_str.html('(' + number_of_dislikes + ')');
+                    $('.video-id-'+data.id).show();
+                    $('.dislikes-video-id-'+data.id).hide();
+                    $('.likes-video-id-'+data.id).hide();
+                    break;
+
+            }
+        })
+    });
+});
