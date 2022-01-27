9396bf957abbdcfba5f8e0bdc325580efda6b343
Lukasz <lukasz199205@gmail.com>
2022-01-27 12:08:59 +0100

    Likes/dislikes saving to database added

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -205,18 +205,42 @@ class FrontController extends AbstractController
 
     private function likeVideo($video)
     {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+        $user->addLikedVideo($video);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
         return 'liked';
     }
     private function dislikeVideo($video)
     {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+        $user->addDislikedVideo($video);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
         return 'disliked';
     }
     private function undoLikeVideo($video)
     {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+        $user->removeLikedVideo($video);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
         return 'undo liked';
     }
     private function undoDislikeVideo($video)
     {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+        $user->removeDislikedVideo($video);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
         return 'undo disliked';
     }
 
