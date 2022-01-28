bc6fa060790be9b498ea52abac2731e43fa05ed6
Lukasz <lukasz199205@gmail.com>
2022-01-28 14:47:01 +0100

    Fix in migration query

--- migrations/Version20220128093127.php
+++ migrations/Version20220128093127.php
@@ -23,7 +23,6 @@ final class Version20220128093127 extends AbstractMigration
         $this->addSql('ALTER TABLE users DROP FOREIGN KEY FK_1483A5E99A1887DC');
         $this->addSql('CREATE TABLE subscriptions (id INT AUTO_INCREMENT NOT NULL, plan VARCHAR(255) NOT NULL, valid_to DATETIME NOT NULL, payment_status VARCHAR(45) DEFAULT NULL, free_plan_used TINYINT(1) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('DROP TABLE subscription');
-        $this->addSql('ALTER TABLE users DROP FOREIGN KEY FK_1483A5E99A1887DC');
         $this->addSql('ALTER TABLE users ADD CONSTRAINT FK_1483A5E99A1887DC FOREIGN KEY (subscription_id) REFERENCES subscriptions (id)');
     }
 
@@ -33,7 +32,6 @@ final class Version20220128093127 extends AbstractMigration
         $this->addSql('ALTER TABLE users DROP FOREIGN KEY FK_1483A5E99A1887DC');
         $this->addSql('CREATE TABLE subscription (id INT AUTO_INCREMENT NOT NULL, plan VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, valid_to DATETIME NOT NULL, payment_status VARCHAR(45) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, free_plan_used TINYINT(1) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('DROP TABLE subscriptions');
-        $this->addSql('ALTER TABLE users DROP FOREIGN KEY FK_1483A5E99A1887DC');
         $this->addSql('ALTER TABLE users ADD CONSTRAINT FK_1483A5E99A1887DC FOREIGN KEY (subscription_id) REFERENCES subscription (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
     }
 }
