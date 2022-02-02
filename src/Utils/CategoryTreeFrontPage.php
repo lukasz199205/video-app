7ba4c9ed2690fcdc35b2a463e6bfc863e9277b6c
Lukasz <lukasz199205@gmail.com>
2022-02-02 09:59:11 +0100

    Route fixes for test after translation setup

--- src/Utils/CategoryTreeFrontPage.php
+++ src/Utils/CategoryTreeFrontPage.php
@@ -36,7 +36,7 @@ class CategoryTreeFrontPage extends CategoryTreeAbstract
 
         foreach ($categories_array as $value) {
             $catName = $this->slugger->slugify($value['name']);
-            $url = $this->urlGenerator->generate('video_list', ['categoryname'=>$catName, 'id'=>$value['id']]);
+            $url = $this->urlGenerator->generate('video_list.en', ['categoryname'=>$catName, 'id'=>$value['id']]);
             $this->categoryList .= $this->html_2. $this->html_3 . $url . $this->html_4 . $value['name']  . $this->html_5;
             if (!empty($value['children'])){
                 $this->getCategoryList($value['children']);
